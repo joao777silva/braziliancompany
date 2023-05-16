556505187aa28f6186a8e936df68729194272a50
joao777silva <joaosilva.lilzinn15@gmail.com>
2023-05-15 21:44:03 -0300

    update prj brazilian company rev.9 > 15/05/2023

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,24 @@
-/* arquivo das automaçoes do menu hamburguer*/
+/* arquivo das automaçoes do menu hamburger*/
+const btnMobile = document.getElementById ('btn-mobile');
+// declarando a variavel com CONST
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') Event.preventDefault ();
+    const nav = document.getElementById('nav');    
+    nav.classList.toggle ('active');
+    const  active = nav.classList.contains(active);
+    event.currentTarget.setAttribute('aria-Expanded', active);
+        if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+    }   else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+
+    
+}
+
+
+    
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
